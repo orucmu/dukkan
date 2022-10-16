ef65a3b998d84772abbeb82b7a24b4832aef99a8
orucmu <mustafaoruc720@gmail.com>
2022-10-16 22:20:57 +0300

    axios'lu

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, Text, FlatList } from "react-native";
 import Config from 'react-native-config';
+import axios from "axios";
 
 
 const Products = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const { data: productData } = await axios.get(Config.API_URL);
+        setData(productData);
+    };
+
+
+    const renderProduct = ({ item }) => <Text>{item.title}</Text>
+
     return (
         <SafeAreaView>
-            <Text>Products{Config.API_URL}</Text>
+            <FlatList data={data} renderItem={renderProduct} />
         </SafeAreaView>
     )
 }
