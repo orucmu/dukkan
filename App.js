b26436ba133c1bddf029b52116948fd465710a23
orucmu <mustafaoruc720@gmail.com>
2022-10-16 20:58:53 +0300

    env

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Config from 'react-native-config';
 
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Home Screen! {Config.API_URL}</Text>
     </View>
   );
 }
